c99b20c80309d00bcba404f8bc43f5f7de69e419
Aaron Tracy <65022882+Devaaron7@users.noreply.github.com>
2025-06-25 09:05:15 -0400

    start commit

---
+++ Models/InventoryItem.cs
@@ -0,0 +1,10 @@
+﻿namespace InventoryApp.Models
+{
+    public class InventoryItem
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Quantity { get; set; }
+        public string Category { get; set; }
+    }
+}
